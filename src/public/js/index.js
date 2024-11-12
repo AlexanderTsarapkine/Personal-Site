1d0ac15da285a89666fc7a6449c3ec5af65a2e31
Alex Tsarapkine <atsarapk@uwaterloo.ca>
2024-11-12 01:14:43 -0500

    added pdf viewer

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,5 +1,7 @@
 let cubeContainer = document.getElementById('cube-container');
 let homePageButtonContainer = document.getElementById('home-page-button-container');
+let resumeSliderContainer = document.getElementById('resume-slider-container');
+
 
 document.getElementById('devButton').addEventListener('click', function() {
     cubeContainer.classList.remove('hidden');
@@ -10,6 +12,14 @@ document.getElementById('devButton').addEventListener('click', function() {
 });
 
 document.getElementById('hrButton').addEventListener('click', function() {
-    cubeContainer.classList.add('hidden');
-    cubeContainer.classList.remove('active');
+    if (resumeSliderContainer.classList.contains('collapsed')) {
+        resumeSliderContainer.classList.remove('collapsed');
+    } else {
+        resumeSliderContainer.classList.add('collapsed');
+    }
+});
+
+document.getElementById('collapse-button').addEventListener('click', function() {
+    const container = document.getElementById('resume-slider-container');
+    container.classList.toggle('collapsed');
 });
